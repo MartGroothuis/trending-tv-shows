8a6a9acea4bcd0c87b4ed1ca8d20787fb8f17db4
Martgroothuis <46526540+Martgroothuis@users.noreply.github.com>
2019-11-29 12:09:57 +0100

    moved the requests to corect file and made list

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -2,8 +2,6 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs/Observable';
-import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 
@@ -16,33 +14,4 @@ import { Tab1Page } from './tab1.page';
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {
-  tvgenres: Observable<any>;
-  tvshows: Observable<any>;
-
-  constructor(private http: HttpClient) { 
-
-    // get all of the genres of the tvshows
-    this.http.get('https://api.themoviedb.org/3/genre/tv/list?api_key=66681a441b89e003faa0833a4ac07f91').subscribe((response) => {
-      this.tvgenres = response;
-    });
-
-    // get all of the trending tv shows of the week
-    this.http.get('https://api.themoviedb.org/3/trending/tv/week?api_key=66681a441b89e003faa0833a4ac07f91&query=genre_ids').subscribe((response) => {
-      this.tvshows = response;
-      this.tvshows.results.forEach(element => {
-        console.log(element);
-
-        element.genre_ids.forEach(element => {
-          const genrename = this.tvgenres.genres.find(
-            genres => genres.id == element
-          ).name;
-          
-          console.log(genrename);
-        });
-      });
-    });
-
-    
-  }
-}
+export class Tab1PageModule {}
