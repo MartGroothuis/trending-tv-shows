603171c07d346d83b702a1e24e404f24132a1714
Martgroothuis <46526540+Martgroothuis@users.noreply.github.com>
2019-11-29 13:37:49 +0100

    displaying the right genres for the tv show

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { TmdbServiceService } from '../services/tmdb-service.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,30 +9,27 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  public tvgenres: any;
-  public tvshows: any;
 
-  constructor(private http: HttpClient) { 
+  public tvgenres: Observable<[]>;
+  public tvshows: Observable<[]>;
+
+  constructor(private tmdbService: TmdbServiceService, private http: HttpClient) { 
 
     // get all of the genres of the tvshows
-    this.http.get('https://api.themoviedb.org/3/genre/tv/list?api_key=66681a441b89e003faa0833a4ac07f91').subscribe((response) => {
-      this.tvgenres = response;
-    });
+    this.tmdbService.genres().subscribe((response: any) => {
+      this.tvgenres = response.genres;
 
-    // get all of the trending tv shows of the week
-    this.http.get('https://api.themoviedb.org/3/trending/tv/week?api_key=66681a441b89e003faa0833a4ac07f91&query=genre_ids').subscribe((response) => {
-      this.tvshows = response;
-      this.tvshows.results.forEach(tvshow => {
-        console.log(tvshow);
+      // get all of the trending tv shows of the week
+      this.tmdbService.trendingTvshows().subscribe((response: any) => {
 
-        // get the name of the genres
-        tvshow.genre_ids.forEach(genre_id => {
-          const genrename = this.tvgenres.genres.find(
-            genres => genres.id == genre_id
-          ).name;
-          
-          console.log(genrename);
-        })
+        this.tvshows = response.results;
+
+        // find the ganre that belongs to the tvshow
+        this.tvshows.map(tvshow => {
+          tvshow.genres = this.tvgenres.filter((genre) => {
+            return tvshow.genre_ids.includes(genre.id);
+          });
+        });
       });
     });
   }
